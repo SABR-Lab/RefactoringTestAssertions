2a1f117e22bb4c9d43712ab212219f98d88734c5
Jesus Valadez <seaofjosus@gmail.com>
2025-03-09 21:42:03 -0500

    Updating main branch with recent implementation

--- src/main/java/org/example/Output.java
+++ src/main/java/org/example/Output.java
@@ -6,7 +6,6 @@ import com.google.gson.GsonBuilder;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Output {
